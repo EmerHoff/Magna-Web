6e5e675a089f43011587d63447c2e6a9e7ac4f4e
Emerson <emerson.hoffmann@marketeasy.store>
2021-04-27 09:29:19 -0300

    alter url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 import { getToken } from "./auth";
 
-const api = Axios.create({ baseURL: 'https://magna-backend.herokuapp.com' });
+const api = Axios.create({ baseURL: 'https://magnafarmapp.loca.lt' });
 //const api = Axios.create({ baseURL: 'http://localhost:3333' });
 
 api.interceptors.request.use(async config => {
