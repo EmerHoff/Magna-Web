24bd743c6a50b7d5acf84163e7b8362eacbdaa2b
EmerHoff <emerson_h2010@hotmail.com>
2021-04-27 22:26:05 -0300

    url local

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 import { getToken } from "./auth";
 
-const api = Axios.create({ baseURL: 'https://magnafarmapp.loca.lt' });
-//const api = Axios.create({ baseURL: 'http://localhost:3333' });
+//const api = Axios.create({ baseURL: 'https://magnafarmapp.loca.lt' });
+const api = Axios.create({ baseURL: 'http://localhost:3333' });
 
 api.interceptors.request.use(async config => {
     const token = getToken();
